845f5580b9a65bbc87c17c3690d5c262c17caab2
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2024-11-03 00:03:13 +0300

    add admin site

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-admin.site.register(Book)
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_filter = [
-        ("title", admin.EmptyFieldListFilter),
-        ("author", admin.EmptyFieldListFilter),
-        ("publication_year", admin.EmptyFieldListFilter)
-    ]
+    list_display = ('title', 'author', 'publication_year') 
+    search_fields = ('title', 'author') 
+    list_filter = ('publication_year',)
+
