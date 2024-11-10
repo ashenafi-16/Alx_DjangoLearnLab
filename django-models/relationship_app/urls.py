39f65bcc21ad3bf99c746b74fa6ebb5adde85e11
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2024-11-10 13:40:16 +0300

    add user interface

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
+from .views import register
+from .views import login_view
+from .views import logout_view
 
 urlpatterns = [
     path('',list_books,name='bookList'),
     path('', LibraryDetailView.as_view(), name='library_detail'),
-
+    path('register/', register, name="register"),
+    path('login/', login_view, name='login'),
+    path('logout',logout_view, name='logout'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib import messages
+from django.contrib.auth import login,logout
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -22,3 +26,35 @@ class LibraryDetailView(DetailView):
         return context
     def get_template_names(self):
         return ['templates/library_detail.html'] 
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created successfully!")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request,'relationship_app/register.html',{'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('register')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+
+    return render(request,'relationship_app/login.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return render(request , 'relationship_app/login.html')
