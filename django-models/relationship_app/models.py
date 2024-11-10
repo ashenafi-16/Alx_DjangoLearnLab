d998270322c47a288db353fb12c7ab011e81118e
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2024-11-10 16:20:50 +0300

    Initial commit

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -14,3 +17,16 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
     library = models.OneToOneField(Library,on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    USER_CHOICES = [
+        ('Admin' , 'Admin'),
+        ('Librarian' , 'Librarian'),
+        ('Member' , 'Member'),
+    ]
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    role = models.CharField(max_length=15,choices=USER_CHOICES,default='MB')
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+        instance.userprofile.save()
