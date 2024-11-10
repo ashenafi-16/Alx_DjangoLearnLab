0800329f05d7f15e7071b92484dbcfee3429a248
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2024-11-10 18:13:14 +0300

    alx week 10 project

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -89,7 +89,7 @@ def add_book(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect to the list of books
+            return redirect('book_list')  
     else:
         form = BookForm()
     return render(request, 'relationship_app/add_book.html', {'form': form})
@@ -101,7 +101,7 @@ def edit_book(request, pk):
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect to the list of books
+            return redirect('book_list') 
     else:
         form = BookForm(instance=book)
     return render(request, 'relationship_app/edit_book.html', {'form': form, 'book': book})
@@ -111,5 +111,5 @@ def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         book.delete()
-        return redirect('book_list')  # Redirect to the list of books
+        return redirect('book_list')  
     return render(request, 'relationship_app/delete_book.html', {'book': book})
