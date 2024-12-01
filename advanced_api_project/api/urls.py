5e8bf2dd9f795bcf9488acb228809851dc4a3ef0
Ashenafi Mulugeta <ashumulugeta016@gmail.com>
2024-12-01 13:53:58 +0300

    advanced api project

---
+++ advanced_api_project/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+urlpatterns = [
+    path('books/', ListView.as_view(), name='list-view'),  
+    path('books/<int:pk>/', DetailView.as_view(), name='detail-view'),  
+    path('books/create/', CreateView.as_view(), name='create-view'),  
+    path('books/<int:pk>/books/update/', UpdateView.as_view(), name='update-view'), 
+    
+    path('books/<int:pk>/books/delete', DeleteView.as_view(), name='delete-view'), 
+]
